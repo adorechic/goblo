04ef653d3a964077b2e4c41797a0e89b986c7a72
adorechic <adorechic@gmail.com>
2016-01-14 21:30:08 +0900

    Post form

--- server.go
+++ server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"net/http"
 	"html/template"
+	"fmt"
 )
 
 type Page struct {
@@ -10,9 +11,16 @@ type Page struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	p := Page{"title"}
-	t := template.Must(template.ParseFiles("top.html"))
-	t.Execute(w, p)
+	r.ParseForm()
+	if r.Method == "POST" {
+		fmt.Println("body", r.Form)
+		fmt.Println("username:", r.Form["username"])
+		fmt.Println("password:", r.Form["password"])
+	} else {
+		p := Page{"title"}
+		t := template.Must(template.ParseFiles("top.html"))
+		t.Execute(w, p)
+	}
 }
 
 func main() {
