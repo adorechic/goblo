d0790b3c2399226d657d89f9b919456b6b3e2cdb
adorechic <adorechic@gmail.com>
2016-01-19 23:16:40 +0900

    Add users table

---
+++ ddl/create_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
+  `name` VARCHAR(64) NOT NULL,
+  `password` VARCHAR(64) NOT NULL,
+  `created_at` TIMESTAMP NOT NULL,
+  `updated_at` TIMESTAMP NOT NULL
+);
