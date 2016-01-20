8204c6eb5bbab1966f797284be9c50e2e50ff25b
adorechic <adorechic@gmail.com>
2016-01-21 01:28:30 +0900

    Insert to sqlite

--- server.go
+++ server.go
@@ -4,10 +4,21 @@ import (
 	"net/http"
 	"html/template"
 	"fmt"
+	"time"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/naoina/genmai"
 )
 
 type Page struct {
 	Title string
+}
+
+type Users struct {
+	Id int64
+	Name string
+	Password string
+	CreatedAt *time.Time
+	UpdatedAt *time.Time
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -26,9 +37,26 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func signupAction(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		r.ParseForm()
-		fmt.Println("username:", r.Form["username"])
-		fmt.Println("password:", r.Form["password"])
 
+		db, err := genmai.New(&genmai.SQLite3Dialect{}, "./development.db")
+		if err != nil {
+			panic(err)
+		}
+		defer db.Close()
+
+		t := time.Now()
+
+		obj := &Users{
+			Name: r.Form["username"][0],
+		  Password: r.Form["password"][0],
+			CreatedAt: &t,
+			UpdatedAt: &t,
+		}
+		n, err := db.Insert(obj)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println("insert:", n)
 	} else {
 		t := template.Must(template.ParseFiles("signup.html"))
 		t.Execute(w, nil)
