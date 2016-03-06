0bb2db279b186ad38b43a982626777307bc3e566
adorechic <adorechic@gmail.com>
2016-03-06 12:26:21 +0900

    Add new page

---
+++ controllers/pages.go
@@ -0,0 +1,15 @@
+package controllers
+
+import (
+	"net/http"
+)
+
+func NewPage(w http.ResponseWriter, r *http.Request) {
+	user, err := currentUser(r)
+	if err != nil {
+		http.Redirect(w, r, "/signin", 301)
+	} else {
+		o := ViewObject{CurrentUser: user}
+		render(w, "pages/new", o)
+	}
+}
