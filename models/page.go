67c98d7690cf9e3be6051d6ff8b45ffabffaaf96
adorechic <adorechic@gmail.com>
2016-03-12 13:02:43 +0900

    Add create method

--- models/page.go
+++ models/page.go
@@ -15,3 +15,26 @@ type Page struct {
 func (p *Page) TableName() string {
 	return "pages"
 }
+
+func CreatePage(title, body string) error {
+	db, err := connect()
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+
+	t := time.Now()
+
+	page := &Page{
+		Title: title,
+		Body: body,
+		CreatedAt: &t,
+		UpdatedAt: &t,
+	}
+	-, err = db..Insert(page)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
