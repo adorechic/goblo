7039ea007f85c9e40e18ba48602e9ad99cf2d953
adorechic <adorechic@gmail.com>
2016-03-13 11:41:49 +0900

    Create page

--- controllers/pages.go
+++ controllers/pages.go
@@ -2,14 +2,30 @@ package controllers
 
 import (
 	"net/http"
+	"github.com/adorechic/goblo/models"
 )
 
 func NewPage(w http.ResponseWriter, r *http.Request) {
 	user, err := currentUser(r)
 	if err != nil {
 		http.Redirect(w, r, "/signin", 301)
-	} else {
+		return
+	}
+
+	if r.Method != "POST" {
 		o := ViewObject{CurrentUser: user}
 		render(w, "pages", o)
+		return
 	}
+
+	title, body := r.Form["title"][0], r.Form["body"][0]
+	err = models.CreatePage(title, body)
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+
+	setFlash(w, r, "Page has created.")
+	http.Redirect(w, r, "/page/" + title, 301)
+	return
 }
